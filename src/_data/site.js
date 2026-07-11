f280d47e88dbca3a947c50121e8d5918b0469403
Bruno Baekelandt <github@baekelandt.net>
2026-07-11 15:39:31 +0200

    Inschrijfformulier naar Apps Script onder buurtcomite.tpleintje@gmail.com
    Nieuwe Sheet + script, volledig eigendom van het buurthuis-account
    (oorzaak van eerdere toegangsproblemen: een browser met meerdere Google-
    accounts tegelijk ingelogd opende links soms onder het verkeerde account).
    Getest en bevestigd werkend.

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,5 +6,5 @@ module.exports = {
   rekening: "BE82 0020 2485 2768",
   rekeninghouder: "Buurthuis 't Pleintje FV",
   // Google Apps Script Web App die inschrijvingen verwerkt (Sheet + mails).
-  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbxoy6G0suu-EdFTG0i0g-k0eS-LibmpKsDA_RYOjKa69gxfSuNVfqPsJls5NKhYLXAZNQ/exec"
+  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbzK5xFxeeJyytNtyB5sG_UOvD1BHQtAmShhyhlrar-e4X8DY1gMGLicqJz8Xy0nYVRR/exec"
 };
