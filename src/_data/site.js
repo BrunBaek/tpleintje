f8cead5f14f83a1772072900f5dbb7e807946ed9
Bruno Baekelandt <github@baekelandt.net>
2026-07-11 14:46:18 +0200

    Inschrijfformulier naar werkende Apps Script-deployment (publiek toegankelijk)
    Vorige deployments stonden nog op "Iedereen met een Google-account" i.p.v.
    "Iedereen, zelfs anoniem", waardoor inschrijvingen van site-bezoekers
    (niet ingelogd) altijd afgewezen werden. Getest en bevestigd werkend.

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,5 +6,5 @@ module.exports = {
   rekening: "BE82 0020 2485 2768",
   rekeninghouder: "Buurthuis 't Pleintje FV",
   // Google Apps Script Web App die inschrijvingen verwerkt (Sheet + mails).
-  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbyNTa817AJ7jEX2inaAz7nPzidgJS0swUc1l51wu5ADOGOuOH11G65rvXi2Zn9uyklPRg/exec"
+  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbxoy6G0suu-EdFTG0i0g-k0eS-LibmpKsDA_RYOjKa69gxfSuNVfqPsJls5NKhYLXAZNQ/exec"
 };
