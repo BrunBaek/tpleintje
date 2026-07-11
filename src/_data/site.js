8300462e8c3e51a6662236194c5a6646af1e7810
Bruno Baekelandt <github@baekelandt.net>
2026-07-11 11:43:08 +0200

    Inschrijfformulier naar nieuwe Apps Script-deployment URL
    Bij het opslaan van het bijgewerkte Apps Script (Opties in mail/Sheet,
    nette bevestigingsmail) ontstond een nieuwe deployment-URL i.p.v. de
    bestaande bij te werken.

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,5 +6,5 @@ module.exports = {
   rekening: "BE82 0020 2485 2768",
   rekeninghouder: "Buurthuis 't Pleintje FV",
   // Google Apps Script Web App die inschrijvingen verwerkt (Sheet + mails).
-  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbwSqG3DIg7tSLvZAzsZxwnvpG54a2hHBKoGBpB0-IgOe0kxVp3uTZJ6JiaSDdmyqFwM8A/exec"
+  inschrijfEndpoint: "https://script.google.com/macros/s/AKfycbwUwLocKxYDRelbV13YCYX-5gAeSf52eRuMZoLQic2KONfy3PNInCXfyFLbsRULVOx_hg/exec"
 };
